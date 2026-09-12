7e22a376fa7c2809e9574bd1a276d929ee844344
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-09-12 03:02:36 -0700

    added gunicorn.conf.py for project 2

---
+++ projects/project-2-dev/gunicorn.conf.py
@@ -0,0 +1,13 @@
+import multiprocessing
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "sync"
+timeout = 30
+keepalive = 2
+
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+
+proc_name = "iris-api"
