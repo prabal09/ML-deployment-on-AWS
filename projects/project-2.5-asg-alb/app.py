a44d7bfbee374bd819c796b8b6d62936db65d0a0
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-09-13 13:01:29 -0700

    added /health to app

--- projects/project-2.5-asg-alb/app.py
+++ projects/project-2.5-asg-alb/app.py
@@ -37,6 +37,9 @@ def predict():
         "class_name": CLASS_MAP.get(class_idx, "unknown"),
     }), 200
 
+@app.route("/health", methods=["GET"])
+def health_check():
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
